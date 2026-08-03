279e44afae0151dd0deedeb57ef05b176d3e7c66
sebastianlwoods <sebastianlwoods@gmail.com>
2026-08-03 01:51:16 +0100

    Load Daily Five styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Anton, Roboto_Condensed } from "next/font/google";
 import "./globals.css";
 import "./magazine-type.css";
+import "./daily-mode.css";
 
 const display = Anton({
   weight: "400",
