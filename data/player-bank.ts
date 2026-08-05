64cd9770373d0e5382082a52049602bba440fea3
sebastianlwoods <sebastianlwoods@gmail.com>
2026-08-05 17:24:50 +0100

    Remove redundant player bank wrapper

--- data/player-bank.ts
+++
@@ -1,6 +0,0 @@
-import { famousPlayers } from "./players-famous";
-import { players as existingPlayers } from "./players";
-
-export type { CareerStop, Player } from "./players";
-
-export const players = [...existingPlayers, ...famousPlayers];
